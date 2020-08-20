3bf53a454712fd245a207c515a8a6f7b84246ee1
”藏田貫司” <kurayan.first@gmail.com>
2020-08-20 05:56:44 +0000

    第二課題

---
+++ task3.php
@@ -0,0 +1,7 @@
+<?php
+//$fruits に配列で好きなフルーツを5個代入し、foreach文で順番に出力してください。
+$fruits = array ("apple","banana","cherry","dragonfruit","peach");
+foreach ($fruits as $fruit){
+    echo $fruit;
+    echo "\n";
+}
